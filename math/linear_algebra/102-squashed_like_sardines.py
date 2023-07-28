a79e925b0d70d6d4b61457381af6b115cc1d0f7f
Javier Icaza <javiicaza@gmail.com>
2023-07-28 13:08:59 -0600

    VM transfer

---
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def cat_matrices(mat1, mat2, axis=0):
+    try:
+        np_mat1 = np.array(mat1)
+        np_mat2 = np.array(mat2)
+
+        if np_mat1.shape[axis] != np_mat2.shape[axis]:
+            return None
+
+        return np.concatenate((np_mat1, np_mat2), axis=axis)
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
