b377f0bc9d2681c8f7624ffd937f38330cb1b584
javicaz <javiicaza@gmail.com>
2023-09-02 20:15:40 -0600

    second try

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python3
 """
-batch optimization to SGD
+Uses batch normalization with tensorflow
 """
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 
 def create_batch_norm_layer(prev, n, activation):
     """
-       Creates a batch normalization layer for a neural network in tensorflow.
-       Args:
-         prev is the activated output of the previous layer
-         n: number of nodes in the layer to be created
-         activation: activation function that should be used on the output
-           of the layer
-       Returns:
-         A tensor of the activated output for the layer.
+    a function that uses batch normalization with tensorflow
+    :param prev: the activated output of the previous layer
+    :param n: the number of nodes in the layer to be created
+    :param activation: the activation function that should be used on the
+    output of the layer
+    :return: a tensor of the activated output for the layer
     """
-    weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layerX = tf.keras.layers.dense(prev, n, kernel_initializer=weights)
-    mean, variance = tf.nn.moments(layerX, 0)
-    gamma = tf.Variable(tf.ones(n), trainable=True)
-    beta = tf.Variable(tf.zeros(n), trainable=True)
-    epsilon = 1e-8
-    batch_norm = tf.nn.batch_normalization(
-        layerX, mean, variance, beta, gamma, epsilon
-    )
-    return activation(batch_norm)
+    # Layers
+    k_init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    output = tf.layers.Dense(units=n, kernel_initializer=k_init)
+    Z = output(prev)
+
+    # Gamma and Beta initialization
+    gamma = tf.Variable(initial_value=tf.constant(1.0, shape=[n]),
+                        name="gamma")
+    beta = tf.Variable(initial_value=tf.constant(0.0, shape=[n]), name="beta")
+
+    # Batch normalization
+    mean, var = tf.nn.moments(Z, axes=0)
+    b_norm = tf.nn.batch_normalization(Z, mean, var, offset=beta,
+                                       scale=gamma,
+                                       variance_epsilon=1e-8)
+    if activation is None:
+        return b_norm
+    else:
+        return activation(b_norm)
